59fc584f58ca06e4ae2e447740a7f28751369cd0
Jifith <jifithjoseph10@gmail.com>
2026-07-23 09:38:41 +0530

    Handsons for week4 and week5

---
+++ Week-03/SpringRest/Exercise4-CountryWebService/Exercise5-GetCountryByCode/src/main/java/com/cognizant/springlearn/Exercise5GetCountryByCodeApplication.java
@@ -0,0 +1,13 @@
+package com.cognizant.springlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise5GetCountryByCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercise5GetCountryByCodeApplication.class, args);
+	}
+
+}
